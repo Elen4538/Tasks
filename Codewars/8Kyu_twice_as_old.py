3a0c132f1a40c4888a08544500e7c723aa10d765
Elena <84009999+Elen4538@users.noreply.github.com>
2021-10-29 11:46:52 +0300

    Update 8Kyu_twice_as_old.py

--- Codewars/8Kyu_twice_as_old.py
+++ Codewars/8Kyu_twice_as_old.py
@@ -1,16 +1,13 @@
-
+#https://www.codewars.com/kata/5b853229cfde412a470000d0
 
 def twice_as_old(a, b):
     
-    diff = abs(a-2*b)
+    diff = abs(a - 2*b)
    
     return diff
 
 
-print(twice_as_old(29, 0))
+#print(twice_as_old(29, 0))
 
-
-#(36,7) 22
-# 55,30 - 5
 
 
